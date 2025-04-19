75a5e151a544730d6873304edd0c03a605a4c091
Keta Jainendra <jainendraketa1@gmail.com>
2025-04-19 12:44:15 +0530

    Template change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,47 +1,30 @@
-import type { Metadata } from 'next'
-import { Inter, Outfit } from 'next/font/google'
+'use client';
 import './globals.css'
-import Navbar from './components/navbar'
-import Footer from './components/footer'
-import ToastContext from './context/toast-context'
-import ActiveSectionContextProvider from './context/section-context'
-import { Analytics } from '@vercel/analytics/react'
+import { Poppins } from '@next/font/google'
+import { ThemeProvider } from 'next-themes'
+import { Analytics } from '@vercel/analytics/react';
 
-const inter = Inter({ subsets: ['latin'] })
-
-const outfit = Outfit({
+const poppins = Poppins({
   subsets: ['latin'],
-  variable: '--font-outfit',
+  weight: ['300', '400', '500', '600', '700', '800'],
+  variable: '--font-poppins'
 })
-
-export const metadata: Metadata = {
-  title: 'Jainendra Ketha',
-  description: 'Portfolio website for Jainendra Ketha.',
-}
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
-    <html
-      lang="en"
-      className="bg-slate-900 scroll-smooth"
-    >
-      <body
-        className={` ${outfit.className} min-h-screen text-gray-50 flex flex-col items-center justify-center overflow-x-hidden w-full`}
-      >
-        <ActiveSectionContextProvider>
-          <Navbar />
-          <ToastContext />
-          <main className="w-full max-w-[1000px] px-4 mt-40 mb-40 flex flex-col gap-32">
-            {children}
-          </main>
-          <Footer />
-        </ActiveSectionContextProvider>
-        <Analytics />
-      </body>
+    <html lang="en">
+      <head />
+      <ThemeProvider attribute='class' defaultTheme='light'>
+        <body className={`${poppins.className} font-poppins bg-gray-100/50 dark:bg-grey-900 text-black dark:text-white overflow-x-hidden`}>
+          {/* <body className='bg-gray-100/50 dark:bg-grey-900 text-black dark:text-white overflow-x-hidden'> */}
+          {children}
+          <Analytics />
+        </body>
+      </ThemeProvider>
     </html>
   )
 }
